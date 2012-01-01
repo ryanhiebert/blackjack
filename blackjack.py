62d33726a5d2ed2f9e6c7e3b55d11306f30457f4
Ryan Hiebert <ryan@ryanhiebert.com>
2012-01-01 01:53:54 -0800

    completely rewrote with classes. not working

--- blackjack.py
+++ blackjack.py
@@ -2,199 +2,234 @@
 from __future__ import print_function, unicode_literals
 import random
 
+try: input, str = raw_input, unicode
+except NameError: pass
+
 SUITS = SPADES, CLUBS, HEARTS, DIAMONDS = '\u2660\u2663\u2665\u2666'
 RANKS = tuple('A23456789') + ('10', 'J', 'Q', 'K')
-
 COLORS = 'BLACK', 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE'
 
-try:
-    input = raw_input
-    str = unicode
-except NameError:
-    pass
+def colorize(text, color, bold=False):
+    template = '\033[{0};{1}m{2}\033[0m'
+    return template.format(int(bold), COLORS.index(color) + 30, text)
 
-def colorize(text, color_name='RED', bold=False):
-    return '\033[{0};{1}m{2}\033[0m'.format(
-        int(bold), COLORS.index(color_name) + 30, text)
+class Player(object):
+    """A player in a BlackJack game"""
+    def __init__(self, pot=None):
+        """set pot and set hands as []"""
+        self.pot, self.hands = pot, []
 
-def score(hand):
-    score, aces = 0, 0
-    for card in hand:
-        rank = card[1:]
-        if rank in tuple('JQK'):
-            score += 10
-        elif rank == 'A':
-            aces += 1
+class Card(str):
+    def colored(self):
+        """Return colored card for printing"""
+        card = self[0] + ' ' + self[1:]
+        if card[0] in (HEARTS, DIAMONDS):
+            card = colorize(card, 'RED')
+        return card
+
+class Hand(list):
+    def score(self):
+        """Count the score of this hand"""
+        score, aces = 0, 0
+        for card in self:
+            rank = card[1:]
+            if rank in RANKS[-3]:
+                score += 10
+            elif rank != 'A':
+                score += RANKS.index(rank) + 1
+        scores = [score + aces]
+        for ace in range(aces):
+            scores.append(scorelist[-1] + 10)
+        return tuple(scores)
+
+    def out(self):
+        """Return a string of the state of the hand, for printing"""
+        out = ''
+        for i, card in enumerate(self):
+            out += card.colored()
+            if i + 1 < len(self):
+                out += ','
+        return out
+
+class Game(object):
+    def __init__(self, decks):
+        """Create a game with a given number of decks and a starting pot"""
+        self.decks = decks
+        self.pot = pot
+        self.reset()
+
+    def reset(self):
+        normal_deck = [Card(suit + rank) for suit in SUITS for rank in RANKS]
+        self.deck = normal_deck * self.decks
+        random.shuffle(self.deck)
+
+    def out(self):
+        """Return a string of the state of the game, for printing"""
+        dealer_out = 'Dealer: ' + str(self.dealer.hands[0].score()) + ' - '
+        dealer_out += self.dealer.hands[0].out()
+        if len(dealer_out) < 30:
+            dealer_out += ('' * (30 - len(dealer_out))) + '  |  '
+        player_out = 'Player: '
+        if len(self.players[0].hands) > 1:
+            for i, hand in enumerate(self.players[0].hands):
+                if i > 0:
+                    player_out += ' ' * (len(dealer_out) + 8)
+                player_out += 'abc'[i] + ': ' + str(hand.score()) + ' - '
+                player_out += hand.out() + '\n'
+            player_out = player_out[:-1] # Remove Final Newline
         else:
-            score += RANKS.index(rank) + 1
-    scorelist = [score + aces]
-    for ace in range(aces):
-        scorelist.append(scorelist[-1] + 10)
-    return tuple(scorelist)
+            player_out += str(self.players[0].hands[0].score()) + ' - '
+            player_out += self.players[0].hands[0].out()
+        return dealer_out + player_out
 
-def colored_card(card):
-    card = card[0] + ' ' + card[1:]
-    if card[0] in SUITS[-2:]:
-        card = colorize(card)
-    return card
-
-def deep(hand):
-    """Checks if a player's hand is deep (has been split)"""
-    return len(hand) > 1 and len(hand[0]) > 1 and len(hand[0][0]) > 1
-
-def out_game(player, dealer):
-    """Write the state of the game to the screen"""
-    dealer_out = 'Dealer: ' + str(score(dealer)) + ' - '
-    for i, card in enumerate(dealer):
-        dealer_out += colored_card(card)
-        if i + 1 < len(dealer):
-            dealer_out += ','
-    len_dealer_out = len(dealer_out)
-    dealer_out += (' ' * (30 - len_dealer_out)) + '  |  '
-    
-    if deep(player):
-        player_out = 'Player: ' + 'a: ' + str(score(player)) + ' - '
-        for i, hand in enumerate(player):
-            if i > 0:
-                player_out += ' ' * (len(dealer_out) + 8)
-            player_out += 'abcd'[i] + ': ' + str(score(hand)) + ' - '
-            for j, card in enumerate(hand):
-                player_out += colored_card(card)
-                if j + 1 < len(hand):
-                    player_out += ','
-            if i + 1 < len(player):
-                player_out += '\n'
-    else:
-        player_out = 'Player: ' + str(score(player)) + ' - '
-        for i, card in enumerate(player):
-            player_out += colored_card(card)
-            if i + 1 < len(player):
-                player_out += ','
-    print(dealer_out + player_out)
-
-while True:
+def get_input(*args, **kwargs):
+    """A wrapper for the input function to exit cleanly on ^D and ^C"""
     try:
-        decks = int(input('How Many Decks? '))
-        if decks < 1:
-            print('Please Enter a Natural Number higher than 0.')
-        else:
-            break
-    except ValueError:
-        print('Please Enter an Integer.')
+        return input(*args, **kwargs)
     except (KeyboardInterrupt, EOFError):
         print()
         raise SystemExit
 
-pot = 100
-while True:
-    deck = [suit + rank for suit in SUITS for rank in RANKS] * decks
-    print('Shuffling the deck')
-    random.shuffle(deck)
+def get_bet(pot):
+    print('Pot: ' + colorize(str(pot), 'GREEN') + '  ', end='')
     while True:
-        player, dealer = [], []
-        print('Pot: ' + str(pot))
-        while True:
-            try:
-                bet = int(input('Place Your Bet: '))
-                if bet > pot:
-                    print('You don\'t have enough in your pot.')
-                elif bet < 0:
-                    print('Please Enter a Natural Number higher than 0.')
-                else:
-                    pot -= bet
-                    print('Pot: ' + str(pot))
-                    break
-            except ValueError:
-                print('Please Enter an Integer')
-            except (KeyboardInterrupt, EOFError):
-                print()
-                raise SystemExit
-        dealer.append(deck.pop())
-        player.append(deck.pop())
-        player.append(deck.pop())
-        out_game(player, dealer)
-        while True:
-            if score(player)[0] > 21 or 21 in score(player):
-                break
-            try:
-                choice = input('(h)it, (s)tand, or (d)ouble down? ')
-            except (KeyboardInterrupt, EOFError):
-                print()
-                raise SystemExit
-            if choice == 'h' or choice == 'H':
-                player.append(deck.pop())
-                out_game(player, dealer)
-            elif choice == 's' or choice == 'S':
-                break
-            elif (choice == 'd' or choice == 'D'):
-                if pot > bet:
-                    pot -= bet
-                    bet *- 2
-                    player.append(deck.pop())
-                    out_game(player, dealer)
-                    break
-                else:
-                    print('You don\'t have enough in your pot.')
+        try:
+            bet = int(get_input('Place Your Bet: '))
+            if bet > pot:
+                print("You don't have enough in your pot.")
+            elif bet < 1:
+                print("You must bet more than 0.")
             else:
-                print('Invalid Selection.')
-        if score(player)[0] > 21:
-            print('Bust!')
-            bet = 0
-        else:
-            if 21 in score(player):
-                print('BlackJack!')
-            dealer.append(deck.pop())
-            if score(dealer)[0] > 21:
-                print('Dealer Bust!')
-            elif 21 in score(dealer):
-                print('Dealer BlackJack!')
+                break
+        except ValueError:
+            print('Please Enter an integer.')
+    return bet
+
+def get_int_input(name, default):
+    while True:
+        try:
+            val = get_input('{0} ({1}): '.format(name, str(default)))
+            if val == '':
+                val = default
             else:
-                for x in score(dealer):
-                    if x <= 21:
-                        dealer_score = x
-                while dealer_score < 17:
-                    dealer.append(deck.pop())
-                    for x in score(dealer):
-                        if x <= 21:
-                            dealer_score = x
-                if 21 in score(dealer):
-                    print('Dealer BlackJack!')
-                elif score(dealer)[0] > 21:
+                val = int(val)
+            if val < 1:
+                print('{0} must be more than 0.'.format(name))
+            else:
+                break
+        except ValueError:
+            print('Please Enter an Integer.')
+    return val
+
+def best_score(hand):
+    best_score = hand.score()[0]
+    for score in hand.score()[1:]:
+        if score <= 21:
+            best_score = score
+    return best_score
+
+if __name__ == '__main__':
+    decks = get_int_input('Number of Decks', 1)
+    pot = get_int_input('Starting Pot', 100)
+
+    game = Game(decks)
+    players, dealer = game.players, game.dealer = [Player(pot)], Player()
+    player, dealer = players[0], dealer
+    
+    while True:
+        # Check if pot is empty
+        if player.pot <= 0:
+            print('Your pot is empty. You Lose. :-(')
+            break
+
+        # Start the game
+        print('Shuffling the deck... ', end='')
+        game.reset()
+        print('done.')
+
+        while True:
+            # Deal
+            player.hands = [Hand([game.deck.pop(), game.deck.pop()])]
+            dealer.hands = [Hand([game.deck.pop()])]
+            bet = get_bet(player.pot)
+            player.hands[0].bet = bet
+            player.pot -= bet
+            print(game.out())
+            while True:
+                # Player's Turn
+                hand = player.hands[0]
+                if hand.score() > 21 or 21 in score:
+                    break
+                choice = get_input('(h)it, (s)tand, or (d)ouble down? ')
+                if choice in tuple('hH'):
+                    hand.append(game.deck.pop())
+                    print(game.out())
+                elif choice in tuple('sS'):
+                    break
+                elif choice in tuple('dD'):
+                    if player.pot > 0:
+                        bet = get_bet(player.pot)
+                        hand.bet += bet
+                        player.pot -= bet
+                        hand.append(game.deck.pop())
+                        print(game.out())
+                        break
+                    else:
+                        print("You don't have anything in your pot.")
+                else:
+                    print('Invalid Selection.')
+
+            if player.hands[0].score()[0] > 21:
+                print('Bust!')
+            else:
+                if 21 in player.hands[0].score():
+                    if len(player.hands[0]) == 2:
+                        print('Blackjack!')
+                    else:
+                        print('21!')
+
+                # Dealer's Turn
+                hand = dealer.hands[0]
+                hand.append(game.deck.pop())
+                if hand.score()[0] > 21:
+                    print(game.out())
                     print('Dealer Bust!')
-            out_game(player, dealer)
-            for x in score(player):
-                if x <= 21:
-                    player_score = x
-            for x in score(dealer):
-                if x <= 21:
-                    dealer_score = x
+                elif 21 in hand.score():
+                    print(game.out())
+                    print('Dealer BlackJack!')
+                else:
+                    while best_score(hand) <= 17:
+                        hand.append(game.deck.pop())
+                    print(game.out())
+                    if 21 in hand.score():
+                        print('Dealer 21!')
+                    elif hand.score()[0] > 21:
+                        print('Dealer Bust!')
+
+            # Score the Game, Retrieve the Bets
+            player_score = best_score(player.hands[0])
+            dealer_score = best_score(dealer.hands[0])
             if player_score == dealer_score:
                 print('Tie Game, bet returned to pot.')
-                pot += bet
-                bet = 0
+                player.pot += player.hands[0].bet
             elif player_score < dealer_score:
                 print('You Lose, better luck next time. Bet forfeit.')
-                bet = 0
+            elif player_score == 21 and len(player.hands[0]) == 2:
+                print('You get your bet plus 1.5x your bet!')
+                player.pot += int(player.hands[0].bet * 2.5)
             else:
-                msg = 'You Win! You bet {0}, so your pot is now {1} richer!'
-                print(msg.format(str(bet), str(bet * 2)))
-
-        if ((decks == 1 and len(deck) < 17) or
-            (decks == 2 and len(deck) < 24) or
-            (decks == 3 and len(deck) < 25)):
-            break
-        elif decks in range(4, 11) and len(deck) < (29 + (decks - 4) * 2):
-            break
-        elif decks > 11 and len(deck) < 42:
-            break
-        else:
-            try:
-                cont = input('Would you like to shuffle the deck? (y/N)')
-            except (KeyboardInterrupt, EOFError):
-                print()
-                raise SystemExit
-            if cont in tuple('yY'):
+                print('You win th bet!')
+                player.pot += player.hands[0].bet * 2
+            
+            # Decide whether to shuffle, or give the player the choice
+            if ((game.decks == 1 and len(game.deck) < 17) or
+                (game.decks == 2 and len(game.deck) < 24) or
+                (game.decks == 3 and len(game.deck) < 25) or
+                (game.decks in range(4,11) and
+                 len(game.deck) < (29 + (game.decks - 4) * 2) or
+                game.decks > 11 and len(game.deck) < 42)):
                 break
-    if pot < 1:
-        print('You are all out of money.  Goodbye :-(')
-        break
+            else:
+                cont = get_input('Shuffle? (y/N) ')
+                if cont in tuple('yY'):
+                    break
